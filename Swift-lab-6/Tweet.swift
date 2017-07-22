3e4b2274a94b6145df143423cfe8688a1e30583e
Redhatfour1 <yaloumessenger@yahoo.com>
2017-07-22 14:58:55 -0700

    finished lab 7

---
+++ Swift-lab-6/Tweet.swift
@@ -0,0 +1,32 @@
+//
+//  Tweet.swift
+//  Swift-lab-6
+//
+//  Created by Louis W. Haywood on 7/8/17.
+//  Copyright © 2017 Louis W. Haywood. All rights reserved.
+//
+
+import Foundation
+
+class Tweet {
+    let text: String
+    let id: String
+    
+    var user: User?
+    
+    init?(json: [String: Any]) {
+        
+        if let text = json["text"] as? String,
+            let id = json["id_str"] as? String,
+            let userJson = json["user"] as? [String: Any] {
+            
+            self.text = text
+            self.id = id
+            self.user = User(json: userJson)
+            
+            
+        } else {
+            return nil
+        }
+    }
+}
